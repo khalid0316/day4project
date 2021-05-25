310157cdd22e38a260633f48c12e20a39a96a29b
khalid0316 <khalid.mowafi@gmail.com>
2021-05-25 19:23:53 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -3,5 +3,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created a new project
+        //enable version control
+        // added .gitignore with .idea target . iml file
+        // made initial commit by keeping track all file
+        // share project in github
     }
 }
